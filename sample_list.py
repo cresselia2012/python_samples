9ae453f8f14d289d64b3d1bb0577aa4325e4ba02
Keiji Kimura <kkimura2317@gmail.com>
2019-10-14 01:35:59 +0900

    make sample_list.py

---
+++ sample_list.py
@@ -0,0 +1,39 @@
+# sample_list.py
+
+# リストを作る
+Mylist = [ 1, 3, 5]
+
+# 空で初期化
+Mylist = []
+#Mylist = list() も同じ
+
+# 要素を追加
+Mylist.append( 1 )
+Mylist.append( 10 )
+Mylist.append( 99 )
+
+# リストの長さ
+print(len(Mylist))
+
+# 中身を表示
+print(Mylist)
+
+# 指定した要素にアクセス
+print(Mylist[1])
+
+# 最後尾の要素にアクセス
+print(Mylist[-1])
+
+# for文で各要素にアクセス
+for x in Mylist:
+    print(x+1)
+
+# 検索
+print( 99 in Mylist )
+
+# 削除
+Mylist.remove( 99 )
+print( 99 in Mylist )
+
+# リスト内包表記
+print([ i*2 for i in range(5)])
